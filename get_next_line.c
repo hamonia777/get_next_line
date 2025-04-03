2551d06aed4417dd09d4f5a3dc4e827108d3f867
Jinwoong Park <jinwpark@c2r11s3.42gyeongsan.kr>
2025-04-03 21:57:40 +0900

    init

--- get_next_line.c
+++ get_next_line.c
@@ -6,95 +6,109 @@
 /*   By: jinwpark <jinwpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 19:24:52 by jinwpark          #+#    #+#             */
-/*   Updated: 2025/04/01 21:38:08 by jinwpark         ###   ########.fr       */
+/*   Updated: 2025/04/02 22:43:20 by jinwpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+static char	*jw_free(char *buf, char **backup)
 {
-	static char	*backup = NULL;
-	char		*buf;
-	ssize_t		bytes_read;
-	char		*tmp;
-	char		*newline_pos;
+	free(buf);
+	if (*backup)
+	{
+		free(*backup);
+		*backup = NULL;
+	}
+	return (NULL);
+}
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	// 1) 버퍼 할당
+static char	*jw_read(int fd, char *backup)
+{
+	char	*buf;
+	char	*tmp;
+	ssize_t	bytes_read;
+
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-
 	bytes_read = 1;
-	// 2) 읽기 루프: backup에 '\n'이 없고, read 반환값이 0이 아닐 동안 반복
 	while (!ft_strchr(backup, '\n') && bytes_read != 0)
 	{
 		bytes_read = read(fd, buf, BUFFER_SIZE);
 		if (bytes_read == -1)
-		{
-			// 읽기 에러 -> 누수 방지를 위해 buf와 backup 해제 후 NULL 반환
-			free(buf);
-			if (backup)
-			{
-				free(backup);
-				backup = NULL;
-			}
-			return (NULL);
-		}
+			return (jw_free(buf, &backup));
 		buf[bytes_read] = '\0';
 		if (!backup)
 			backup = ft_strdup(buf);
 		else
 		{
-			// 기존 backup + 새로 읽은 buf 병합
 			tmp = ft_strjoin(backup, buf);
 			free(backup);
 			backup = tmp;
 		}
 	}
 	free(buf);
-	// 3) backup이 NULL이거나 아무 내용 없으면(EOF 상황), NULL 반환
+	return (backup);
+}
+
+static char	*jw_copy(const char *src, size_t len)
+{
+	char	*line;
+	size_t	i;
+
+	i = 0;
+	line = malloc(len + 1);
+	while (i < len)
+	{
+		line[i] = src[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*jw_extract(char **backup)
+{
+	char	*newline;
+	char	*line;
+	char	*tmp;
+	size_t	len;
+
+	newline = ft_strchr(*backup, '\n');
+	if (newline)
+	{
+		len = newline - *backup + 1;
+		line = jw_copy(*backup, len);
+		tmp = ft_strdup(newline + 1);
+		free(*backup);
+		*backup = tmp;
+		if (*backup && !(*backup)[0])
+		{
+			free(*backup);
+			*backup = NULL;
+		}
+	}
+	else
+	{
+		line = *backup;
+		*backup = NULL;
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*backup;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	backup = jw_read(fd, backup);
 	if (!backup || backup[0] == '\0')
 	{
 		free(backup);
 		backup = NULL;
 		return (NULL);
 	}
-	// 4) backup 안에 newline이 있는지 확인
-	newline_pos = ft_strchr(backup, '\n');
-	if (newline_pos)
-	{
-		// 개행 문자 포함 줄 반환
-		size_t	len = newline_pos - backup + 1; // '\n'까지 길이
-		char	*line = (char *)malloc(sizeof(char) * (len + 1));
-		size_t	i = 0;
-		if (!line)
-			return (NULL);
-		while (i < len)
-		{
-			line[i] = backup[i];
-			i++;
-		}
-		line[i] = '\0';
-		// 나머지 문자열을 tmp에 복사해 backup 대체
-		tmp = ft_strdup(newline_pos + 1);
-		free(backup);
-		backup = tmp;
-		// 만약 남은 문자열이 비어있다면 free 후 NULL
-		if (backup && backup[0] == '\0')
-		{
-			free(backup);
-			backup = NULL;
-		}
-		return (line);
-	}
-	else
-	{
-		// 개행 문자가 없음 -> 남은 내용이 마지막 줄
-		char *line = backup;
-		backup = NULL; // backup 해제
-		return (line); // line은 호출자가 free해야 함
-	}
+	return (jw_extract(&backup));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jinwpark <jinwpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:44:48 by jinwpark          #+#    #+#             */
-/*   Updated: 2025/04/01 21:18:41 by jinwpark         ###   ########.fr       */
+/*   Updated: 2025/04/02 21:53:07 by jinwpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-#include <limits.h>
+# include <limits.h>
 
 size_t	ft_strlen(const char *str);
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
